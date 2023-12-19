bb134fb372f6ef2facb0d2a9470f8042e86d6bed
Vivare <vivareveritas@gmail.com>
2023-12-19 03:35:52 -0500

    got the most basic functionality working

--- src/extension.ts
+++ src/extension.ts
@@ -20,6 +20,18 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+    let myFormattingProvider = vscode.languages.registerDocumentFormattingEditProvider('overpy', {
+        provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+            const firstLine = document.lineAt(0);
+            if (firstLine.text !== '42') {
+                return [vscode.TextEdit.insert(firstLine.range.start, '42\n')];
+            }
+			return [];
+        }
+    });
+
+	context.subscriptions.push(myFormattingProvider);
 }
 
 // This method is called when your extension is deactivated
